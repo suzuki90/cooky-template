898311bfd4ab0d39388d5d5c503a202fdf84b341
suzuki90 <suzuki.no.maiadr@gmail.com>
2012-12-21 18:12:44 +0900

    サンプルを作成

---
+++ test/010_sample.js
@@ -0,0 +1,53 @@
+module.exports.args = {
+    fileDir : './',
+};
+var CookyTemplate = require('cooky-template');
+
+// テンプレートファイル名（module.exports.args.fileDir以降）
+var tplFile = '010_sample.tpl';
+
+// テンプレートパラメタ
+var tplParam = {
+    title : 'CookyTemplate',
+    say   : 'hello!',
+    money : 10000,
+    flag  : true,
+    flag2 : false,
+    list  : ['taro', 'jiro', 'hanako'],
+    info  : [
+        {key:'name', value:'cooky'},
+        {key:'version', value:'1.0.0'},
+        {key:'desc', value:'Template engine in node.js'},
+    ],
+};
+
+// テンプレート関数
+var tplFuncsName = 'echo, echoAsync';
+var tplFuncsArray = [
+    // echo
+    function(cc, callback, arg){
+        callback(null, arg);
+    },
+    // echoAsync
+    function(cc, callback, arg){
+        process.nextTick(function(){
+            callback(null, arg);
+        });
+    },
+];
+
+// 解析
+CookyTemplate.compile(
+    tplFile,
+    null, // 必要ならcooky-chainを渡す
+    tplParam,
+    tplFuncsName,
+    tplFuncsArray,
+    function(err, warns, data){ // callback
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if (warns) console.log(warns);
+        console.log(data);
+});
---
+++ test/010_sample.tpl
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <meta charset="utf8">
+    <title>[% title %]</title>
+</head>
+<body>
+
+    <h1>Example</h1>
+
+    <!-- PARAM -->
+    <br />
+    say "[% say %]"<br />
+    money : [% money | comma %]<br />
+
+    <!-- FUNC -->
+    <br />
+    [% echo('this is echo') %]<br />
+    [% echoAsync('this is echoAsync') %]<br />
+
+    <!-- IF -->
+    <br />
+    flag is 
+    [% IF flag %]
+        true
+        [% IF flag2 %]
+            -> true
+        [% ELSE %]
+            -> false
+        [% /IF %]
+    [% ELSE %]
+        false
+        [% IF flag2 %]
+            -> true
+        [% ELSE %]
+            -> false
+        [% /IF %]
+    [% /IF %]
+    <br />
+
+    <!-- FOR（配列）-->
+    <br />
+    [% FOR value IN list %]
+        - [% loop.count %] : [% value %]<br />
+    [% /FOR %]
+
+    <!-- FOR（連想配列）-->
+    <br />
+    [% FOR data IN info %]
+        [% data.key %] : [% data.value %]<br>
+    [% /FOR %]
+
+</body>
+</html>
